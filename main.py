72bc9705191f5c3927dda2926338b7124a4c19c0
jfold <jonathan.foldager@gmail.com>
2022-03-14 13:54:54 +0100

    update main

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-from src.gp_lvm import GPLVM
+from imports.general import *
+from src.parameters import Parameters
+from src.experiment import Experiment
+
+
+def run():
+    start = time.time()
+    try:
+        args = sys.argv[-1].split("|")
+    except:
+        args = []
+    print("------------------------------------")
+    print("Arguments:", args)
+    print("RUNNING EXPERIMENT...")
+    kwargs = {}
+    parameters_temp = Parameters(mkdir=False)
+    if args[0] != "main.py":
+        for arg in args:
+            var = arg.split("=")[0]
+            val = arg.split("=")[1]
+            par_val = getattr(parameters_temp, var)
+
+            if isinstance(par_val, bool):
+                val = val.lower() == "true"
+            elif isinstance(par_val, int):
+                val = int(val)
+            elif isinstance(par_val, float):
+                val = float(val)
+            elif isinstance(par_val, str):
+                pass
+            else:
+                var = None
+                print("COULD NOT FIND VARIABLE:", var)
+            kwargs.update({var: val})
+
+    parameters = Parameters(kwargs, mkdir=True)
+    print("Running with:", parameters)
+    experiment = Experiment(parameters)
+    experiment.run()
+    print("FINISHED EXPERIMENT")
+    print("------------------------------------")
+
+
+if __name__ == "__main__":
+    run()
